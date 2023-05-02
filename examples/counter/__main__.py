3f99570a4239c4436ef30cd3344befda6aca8978
Matteo Merz <mtt.merz@gmail.com>
2023-05-02 22:28:40 +0200

    chore: upgrade counter example

---
+++ examples/counter/__main__.py
@@ -0,0 +1,45 @@
+import random
+from threading import Thread
+import time
+
+# Multiple purposes:
+#   (1) declare the available actor instances
+#   (2) count instance invocations
+invocations_counter = {
+    'ASDFG': 0,
+    'QWERT': 0,
+    'POIUY': 0,
+    'LKJHG': 0,
+    'MNBVC': 0,
+    'ZXCVB': 0
+}
+
+# Record starting time, for logging purposes
+start_time = time.time()
+
+# 30 is the max number of concurrent invocations
+# 60 is the max number of invocations each minute
+n = 20
+
+# Invoke n times the 'increment' operation on a random actor
+# To test the concurrency, each invokation is sent on a new thread
+threads = []
+for i in range(n):
+    id = random.choice(list(invocations_counter))
+    invocations_counter[id] += 1
+
+    t = Thread(target=invoke, args=[id, 'increment'])
+    t.start()
+
+    threads.append(t)
+
+# Wait for thread execution
+for t in threads:
+    t.join()
+
+# Print expected results
+for i in invocations_counter.items():
+    print('Actor@{} invoked {} times'.format(i[0], i[1]))
+
+print('\nInvocations count: {}'.format(n))
+print('Execution time: ~{} seconds'.format(int(time.time() - start_time)))
