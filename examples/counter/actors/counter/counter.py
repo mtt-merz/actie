c4b3deb62648ce55dd3716fefc77cbd0c1341281
mtt-merz <mtt.merz@gmail.com>
2023-10-30 15:47:05 +0100

    fix: remove old required params in Counter example

--- examples/counter/actors/counter/counter.py
+++ examples/counter/actors/counter/counter.py
@@ -2,8 +2,7 @@ from lib import Actor
 
 
 class Counter(Actor):
-    def __init__(self, name: str) -> None:
-        self.id = name
+    def __init__(self) -> None:
         self.value = 0
 
     def receive(self, msg: str) -> str:
@@ -11,8 +10,8 @@ class Counter(Actor):
 
         if msg == 'increment':
             self.value += 1
-            if (self.value < 5):
-                self.send(self.name, self.id, msg)
+            # if (self.value < 5):
+            #     self.send(self.family, self.name, msg)
 
         elif msg == 'decrement':
             self.value -= 1
