c29468d5f25c12669bf289a7a88f91533d79b27f
mtt-merz <mtt.merz@gmail.com>
2023-11-25 16:26:42 +0100

    add data

--- examples/focus_feed/stats/_aggregate.py
+++ examples/focus_feed/stats/_aggregate.py
@@ -14,38 +14,38 @@ from logger import Logger
 # aggregate with user policy=1000
 # 
 # - aggregate/BASE__2023-11-25_09-54-18.csv
-# - aggregate/.csv (persist)
-# - aggregate/.csv (no persist)
+# - aggregate/ACTIE__2023-11-25_16-10-50.csv (persist)
+# - aggregate/ACTIE__2023-11-25_16-03-54.csv (no persist)
 
 # EXPERIMENT #2
 # aggregate with user policy=100
 # 
 # - aggregate/BASE__2023-11-25_09-59-31.csv
-# - aggregate/.csv (persist)
-# - aggregate/.csv (no persist)
+# - aggregate/ACTIE__2023-11-25_16-15-25.csv (persist)
+# - aggregate/ACTIE__2023-11-25_16-18-35.csv (no persist)
 
 # EXPERIMENT #3 
 # aggregate with user policy=1
 #
 # - aggregate/BASE__2023-11-25_10-04-03.csv
-# - aggregate/.csv (persist)
-# - aggregate/.csv (no persist)
+# - aggregate/ACTIE__2023-11-25_16-24-01.csv (persist)
+# - aggregate/ACTIE__2023-11-25_16-21-28.csv (no persist)
 
 console = Console(
     name='aggregate',
-    implementation=Implementation.base
+    implementation=Implementation.actie
 )
 
 for i in range(100):
     res = console.aggregate(
         topic=f"topic",
-        user=f"luigi",
+        user=f"user",
         log_args=LogArgs(
             topics=1,
             users=1,
-            articles=1000,
+            articles=1,
             subcriptions=1,
-            persist=False
+            persist=True
         )
     )
 
