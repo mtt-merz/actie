e59dad894e507e2ba744d8b99d7cb2a06ac872da
mtt-merz <mtt.merz@gmail.com>
2023-11-25 12:02:53 +0100

    add data

--- examples/focus_feed/stats/_subscribe.py
+++ examples/focus_feed/stats/_subscribe.py
@@ -3,25 +3,42 @@ from console import Console, Implementation, LogArgs
 from logger import Logger
 
 
-
 # EXPERIMENT #1:
-# a user subscribe every 2 sec to a different topic, for 500 times 
+# a user subscribe every 2 sec to a different topic, for 500 times
 # (scale the number of topics)
-# 
+#
 # - logs/subscribe/BASE__2023-11-25_07-26-07.csv (1->500)
-# - logs/subscribe/ACTIE__2023-11-25_10-24-34.csv
+# - logs/subscribe/ACTIE__2023-11-25_10-24-34.csv (no persist)
+# - logs/subscribe/ACTIE__2023-11-25_11-23-09.csv (persist) (1->500)
 
 # EXPERIMENT #2:
-# a different user subscribe every 2 sec to a topic, for 500 times 
+# a different user subscribe every 2 sec to a topic, for 500 times
 # (scale the number of topics)
-# 
+#
 # - logs/subscribe/BASE__2023-11-25_07-26-07.csv (500 -> 1000)
-# - logs/
+# - logs/subscribe/ACTIE__2023-11-25_10-47-20.csv (no persist)
+# - logs/subscribe/ACTIE__2023-11-25_11-23-09.csv (persist) (500->1000)
 
 console = Console(
     name='subscribe',
     implementation=Implementation.actie
 )
+
+for i in range(500):
+    res = console.subscribe(
+        topic=f"topic{i}",
+        user=f"user",
+        log_args=LogArgs(
+            topics=i,
+            users=1,
+            articles=0,
+            subcriptions=i,
+            persist=True
+        )
+    )
+
+    print(f'{i} -> {res}')
+    time.sleep(2)
 
 for i in range(500):
     res = console.subscribe(
@@ -32,7 +49,7 @@ for i in range(500):
             users=i,
             articles=0,
             subcriptions=i,
-            persist=False
+            persist=True
         )
     )
 
