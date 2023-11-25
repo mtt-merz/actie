6e673f416d91faf52d87cea5e9b1ec6d7d8c2820
mtt-merz <mtt.merz@gmail.com>
2023-11-25 10:47:04 +0100

    add data

--- examples/focus_feed/stats/_subscribe.py
+++ examples/focus_feed/stats/_subscribe.py
@@ -2,27 +2,34 @@ import time
 from console import Console, Implementation, LogArgs
 from logger import Logger
 
-# a user subscribe every 1 sec to a different topic, for 1000 times
 
-# EXPERIMENT #1 - logs/subscribe/BASE__2023-11-25_07-26-07.csv
-# db usage notes |  07:20    07:51     |
-# -------------------------------------|  
-# DISK READ:     |  0B       0B        |
-# DISK WRITE:    |  418KB    696KB     |
-# memory usage:  |  27.05MB  29.36MB   |
+
+# EXPERIMENT #1:
+# a user subscribe every 2 sec to a different topic, for 500 times 
+# (scale the number of topics)
+# 
+# - logs/subscribe/BASE__2023-11-25_07-26-07.csv (1->500)
+# - logs/subscribe/ACTIE__2023-11-25_10-24-34.csv
+
+# EXPERIMENT #2:
+# a different user subscribe every 2 sec to a topic, for 500 times 
+# (scale the number of topics)
+# 
+# - logs/subscribe/BASE__2023-11-25_07-26-07.csv (500 -> 1000)
+# - logs/
 
 console = Console(
     name='subscribe',
-    implementation=Implementation.base
+    implementation=Implementation.actie
 )
 
-for i in range(1000):
+for i in range(500):
     res = console.subscribe(
-        topic=f"topic{i}",
-        user=f"luigi",
+        topic=f"topic",
+        user=f"user{i}",
         log_args=LogArgs(
-            topics=i,
-            users=1,
+            topics=1,
+            users=i,
             articles=0,
             subcriptions=i,
             persist=False
@@ -30,4 +37,4 @@ for i in range(1000):
     )
 
     print(f'{i} -> {res}')
-    time.sleep(1)
+    time.sleep(2)
